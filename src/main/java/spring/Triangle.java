f3fb318bb742d43c9c6719d26b9e6607c6b3015d
Ravi Kumar <ravikumarkarn080@gmail.com>
2019-06-01 14:22:38 +0530

    Bean with property

--- src/main/java/spring/Triangle.java
+++ src/main/java/spring/Triangle.java
@@ -1,15 +1,24 @@
 package spring;
 
 public class Triangle {
-    public Triangle() {
+    private String type;
+
+    public String getType() {
+        return type;
     }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Triangle() {}
 
     @Override
     public String toString() {
-        return "Triangle{}";
+        return this.type + " Triangle";
     }
 
     public void draw() {
-        System.out.println("Draw triangle");
+        System.out.println("Draw triangle " + this.type);
     }
 }
